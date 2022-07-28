269d5b3616acc0e88bd2ce9ef9bcb1ab7b6965ad
James Hochadel <james.hochadel@gsa.gov>
2022-07-28 02:23:50 -0400

    Allow running on cloud foundry with http endpoints

--- ci/smoke-tests/aws-rds/update-storage-full/main.go
+++ ci/smoke-tests/aws-rds/update-storage-full/main.go
@@ -7,12 +7,17 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	// init the postgres driver
 	_ "github.com/lib/pq"
 )
+
+var databaseFull bool
+var records int64
 
 func randomString(length int32) string {
 	s := make([]int32, length)
@@ -65,39 +70,67 @@ func config(ctx context.Context, timeout time.Duration) (configuration, error) {
 	return c, nil
 }
 
-// main populates a database with random data until it is full, then dumps the
-// database contents to an s3 bucket for later use in smoke tests. It does not
-// run with the smoke tests; it should only need to be re-run if the testdata
-// bucket or its contents are deleted.
+func fillDatabase(ctx context.Context, db *sql.DB) {
+	var err error
+	if _, err := db.Exec(`DROP TABLE IF EXISTS data; CREATE TABLE data(t text);`); err != nil {
+		log.Fatal("dropping and recreating table", err)
+	}
+
+	for err == nil {
+		s := randomString(1024 * 1024) // 1MB per row
+		_, err = db.ExecContext(ctx, `INSERT INTO data(t) VALUES ($1);`, s)
+		records++
+	}
+
+	if strings.Contains(err.Error(), "No space left on device") {
+		databaseFull = true
+		log.Printf("Stopped filling database. Filled %v rows. Error was: %v\n", records, err.Error())
+	}
+}
+
+func logUpdates(ctx context.Context, db *sql.DB) {
+	t := time.NewTicker(5 * time.Second)
+	for {
+		select {
+		case t := <-t.C:
+			log.Printf("time: %v. records inserted: %v.", t, records)
+		case <-ctx.Done():
+			return
+		}
+	}
+}
+
+func newDB(ctx context.Context) *sql.DB {
+	c, err := config(ctx, 30*time.Second)
+	if err != nil {
+		log.Fatal("loading configuration:", err)
+	}
+	db, err := sql.Open("postgres", c.databaseURI)
+	if err != nil {
+		log.Fatal("opening connection:", err)
+	}
+	return db
+}
+
+// main populates a database with random data until it is full, and serves an endpoint that indicates whether the database has been filled.
 //
 // Standard postgresql connection variables must be set on the environment for
 // this script to run. See https://www.postgresql.org/docs/14/libpq-envars.html.
 func main() {
-	// 10 minute overall timeout.
-	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(2*time.Minute))
+	// Set an overall timeout for the insert operation.
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(16*time.Minute))
 	defer cancel()
 
-	c, err := config(ctx, 30*time.Second)
-	if err != nil {
-		log.Fatal(err)
-	}
-	db, err := sql.Open("postgres", c.databaseURI)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if _, err := db.Exec(`CREATE TABLE IF NOT EXISTS data(t text);`); err != nil {
-		log.Fatal(err)
-	}
+	db := newDB(ctx)
+	defer db.Close()
+	go fillDatabase(ctx, db)
+	go logUpdates(ctx, db)
 
-	var rows int64
-	for err == nil {
-		s := randomString(1024 * 1024) // 1MB per row
-		_, err = db.ExecContext(ctx, `INSERT INTO data(t) VALUES ($1);`, s)
-		rows++
-	}
-
-	// todo figure out the 'storage full' error and exit 0 if it's that, but otherwise exit 1
-	log.Printf("Stopped filling database. Filled %v rows. Error was: %v\n", rows, err.Error())
-
-	os.Exit(0)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("cloud foundry status check OK\n"))
+	})
+	http.HandleFunc("/db", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(fmt.Sprintf("%v\n", databaseFull)))
+	})
+	fmt.Println(http.ListenAndServe(":8080", nil))
 }
